0fb8de5e0091b8f97cd0d78ec4d1c7eb7377cec0
Morganebodin <morganebodin@gmail.com>
2026-08-21 16:38:50 +0200

    exercice 1 complet

--- ex1.c
+++ ex1.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
+
+void ajouter_entiers(void){
+int a = 0;
+int b =00;
+printf("entrer 2 chiffres\n");
+scanf("%d",&a);
+scanf("%d",&b);
+int c = a +b;
+printf("%d\n",c);
+
+}
+
+void ajouter_flottants(void){
+float a = 0;
+float b =0;
+printf("entrer 2 chiffres a virgules\n");
+scanf("%f",&a);
+scanf("%f", &b);
+float c = a+b;
+printf("%f", c);
+}
+
+
 void exercice1( void )
 {
-/* ... */
+ajouter_entiers();
+ajouter_flottants();
 }
 void exercice2( void )
 {
-/* ... */
+
 }
-int main( int argc, char* argv[] )
+int main( int/*argc*/, char** /*argv*/ )
 {
 exercice1();
 exercice2();

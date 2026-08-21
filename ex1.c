2c6e76dde5982a3545dca6dc07bc1cfb3a330a95
Morganebodin <morganebodin@gmail.com>
2026-08-21 16:12:02 +0200

    exercice 1 vide

---
+++ ex1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void exercice1( void )
+{
+/* ... */
+}
+void exercice2( void )
+{
+/* ... */
+}
+int main( int argc, char* argv[] )
+{
+exercice1();
+exercice2();
+return 0;
+}

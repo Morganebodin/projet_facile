9dc944d57c7686df2247c2f6c2ccb4b6f3c2a869
Morganebodin <morganebodin@gmail.com>
2026-08-23 15:42:30 +0200

    exercice1 terminé

--- tp2.c
+++ tp2.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
-void exercice1 (void)
+void exercice1(void)
 {
-int nombre = 0;
-printf("entrer un nombre:");
-scanf("%d"
-
-  return;
+	int nombre = 0;
+	int maximum =0;
+	printf("entrer un nombre:\n");
+	scanf("%d", &nombre);
+	maximum = nombre;
+	while (nombre != 0) {
+		printf("entrer un nouveau nombre\n");
+		scanf("%d",&nombre);
+	if (nombre > maximum){
+	maximum = nombre;
+	}
 }
-int main (void){
 
-  exercice1();
-  return 0;
+	printf("le chiffre est: %d\n",maximum);
+
+	return;			//ligne optionnelle
 }
 
+int main(void)
+{
+
+	exercice1();
+	return 0;
+}

286b0e4cbab3b41d4ea8833e7b2a697f9c855505
Morganebodin <morganebodin@gmail.com>
2026-08-30 15:12:00 +0200

    feat: ex3 finished

--- TPUNIX/TP2/tp3ex2.c
+++ TPUNIX/TP2/tp3ex2.c
@@ -1,27 +1,46 @@
 #include <stdio.h>
-#include <string.h> // Indispensable pour strlen
+#include <string.h>		// Indispensable pour strlen
+#include <ctype.h>		// pour toupper
 
-void miroir(char *src, char *dest) {    
+void miroir(char *src, char *dest)
+{
 
-int longueur = strlen(src);
+	int longueur = strlen(src);
 
-    for(int i = 0; i<longueur ; i++){
-dest[0+i] = src [longueur -1-i];
+	for (int i = 0; i < longueur; i++) {
+		dest[0 + i] = src[longueur - 1 - i];
+
+	}
+
+	dest[longueur] = '\0';
 
 }
 
-dest[longueur] = '\0';
+void majuscule(char *src, char *dest)
 
+{
+int i = 0;
+	while ( src[i] != 0) {
+		dest[i] = toupper(src[i]);
+		i++;
+	}
+	dest[i] = '\0';
 }
 
-int main(void) {
-    char* source = "polycopie";
-    char destination[100];
+int main(void)
+{
+	char *source = "polycopie";
 
-    miroir(source, destination);
+	char destination[100];
+	//char storagearg2[100];
+	// miroir(source, destination);
+	// miroir("avril", destination);
+	// miroir("argument1", storagearg2);
 
-    printf("Original : %s\n", source);
-    printf("Miroir   : %s\n", destination);
+	majuscule(source, destination);
 
-    return 0;
+	printf("Original : %s\n", source);
+	printf("Miroir   : %s\n", destination);
+
+	return 0;
 }

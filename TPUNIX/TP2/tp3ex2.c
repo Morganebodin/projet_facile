cd439f24832d52480ccceba27a28af146f4a503b
Morganebodin <morganebodin@gmail.com>
2026-08-30 14:42:56 +0200

    feat: tp3 ex2 explained

--- TPUNIX/TP2/tp3ex2.c
+++ TPUNIX/TP2/tp3ex2.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <string.h> // Indispensable pour strlen
 
-void miroir(char *src, char *dest) {
-    int longueur = strlen(src);
-    int j = 0; // Indice pour remplir dest
+void miroir(char *src, char *dest) {    
 
-for (int i = longueur -1; i >=0; i--){
-	dest[j] = src[i];
-	j++;
-}    // Écris ici ta boucle for ou while pour lire src de la fin vers le début
-    // N'oublie pas de fermer dest avec '\0'
-dest[j] = '\0';
+int longueur = strlen(src);
+
+    for(int i = 0; i<longueur ; i++){
+dest[0+i] = src [longueur -1-i];
+
+}
+
+dest[longueur] = '\0';
 
 }
 
 int main(void) {
-    char source[] = "polycopie";
-    char destination[100]; 
+    char* source = "polycopie";
+    char destination[100];
 
     miroir(source, destination);
 

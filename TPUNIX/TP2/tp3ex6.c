ae26f14b52c27f99fba697e24b1957cd1bd4f1c2
Morganebodin <morganebodin@gmail.com>
2026-09-03 11:06:58 +0200

    feat tp3ex6 en cours

---
+++ TPUNIX/TP2/tp3ex6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void estContenu(char* element, char* s){
+
+int i = 0;
+int j = 0;
+while (s[i] != '\0'){
+    return 1;
+while element[j] == s[i+j]{
+    if (element[j] != '\0')
+    j++;
+
+    return 0;
+}
+
+}
+}
+int main (){
+
+    char* p1 ="polycopie";
+    char* mot1 = ""
+    estContenu();
+
+    return 0;
+}
